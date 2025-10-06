5957c74709b6c63b65337d032af815eabd772175
Krys-Salazar-Ch <krisachava@gmail.com>
2025-10-06 16:14:23 -0600

    Tabla agente y todas las demas pequeñas
    Tabla agente y todas las demas pequeñas

--- schema/03-tables-propiedad.sql
+++ schema/03-tables-propiedad.sql
@@ -17,6 +17,20 @@ CREATE TABLE Propiedad (
 );
 GO
 
+-- TABLA ESTADO PROPIEDAD
+CREATE TABLE EstadoPropiedad (
+    idEstadoPropiedad INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL
+)
+GO
+
+-- TABLA TIPO INMUEBLE
+CREATE TABLE TipoInmueble (
+    idTipoInmueble INT IDENTITY(1,1) PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL
+)
+GO
+
 -- agregamos validaciones
 
 -- ALTER TABLE Propiedad
